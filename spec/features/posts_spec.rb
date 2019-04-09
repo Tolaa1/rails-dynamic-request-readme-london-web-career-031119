9200ae0146bbb3e1c4f74b6fccf26a48d4032856
Tola <you@example.com>
2019-04-09 11:41:26 +0100

    Done.

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -10,10 +10,12 @@ describe 'navigate' do
     expect(page.status_code).to eq(200)
   end
 
+
   it 'shows the title on the show page in an h1 tag' do
     visit "/posts/#{@post.id}"
     expect(page).to have_css("h1", text: "My Post")
   end
+
 
   it 'shows the description on the show page in a p tag' do
     visit "/posts/#{@post.id}"
